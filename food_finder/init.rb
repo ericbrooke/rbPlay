e9c4ba80b0ea5d774c491af37043352f93b43862
Eric Brooke <ericbrooke@mac.com>
2014-01-14 19:23:57 -0800

    initial commit

---
+++ food_finder/init.rb
@@ -0,0 +1,10 @@
+# Food Finder
+#Launch this file from command line
+
+APP_ROOT = File.dirname(__FILE__) #absolute path
+
+# require "#{APP_ROOT}/lib/guide"
+# require File.join(APP_ROOT, 'lib', 'guide.rb')	will work in windows and unix
+
+$:.unshift(File.join(APP_ROOT, 'lib'))
+require 'guide'
---
+++ food_finder/lib/guide.rb
@@ -0,0 +1,18 @@
+class Guide
+
+  def initialize(path=nil)
+  	# locate restaurant file
+  	# or create new 
+  	# exit if fails
+  end
+
+  def launch!
+    #introduction
+    #action loop
+    #what do you want to do
+    #do that action
+    # repat untils user quits
+    # fond farewell
+  end
+
+end
