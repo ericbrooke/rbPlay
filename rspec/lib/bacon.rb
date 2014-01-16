7c88ecfc90c8eebda176b8458f1acb2f4ad061f8
Eric Brooke <ericbrooke@mac.com>
2014-01-15 20:06:56 -0800

    building spec for Bacon

---
+++ rspec/lib/bacon.rb
@@ -0,0 +1,13 @@
+class Bacon
+
+	attr_accessor :expired
+
+	def edible?
+		!expired
+	end
+
+	def expired!
+		self.expired = true
+	end
+
+end
---
+++ rspec/spec/bacon_spec.rb
@@ -0,0 +1,15 @@
+require 'bacon'
+
+describe Bacon do 
+
+  it "is edible" do
+  	expect(Bacon.new.edible?).to be_true
+  end
+
+  it "expired!" do
+    bacon= Bacon.new
+    bacon.expired!
+    expect(bacon).to_not be_edible
+  end
+
+end
