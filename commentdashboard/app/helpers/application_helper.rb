81034d9379b65e7185ff3105ed765dbb68457100
Eric Brooke <ericbrooke@mac.com>
2014-01-19 12:54:56 -0800

    check masthead, title and added application helpers

--- commentdashboard/app/helpers/application_helper.rb
+++ commentdashboard/app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+	def title
+		"Comments Dashboard"
+	end
+
+	def subtitle
+		"Read comments from your favourite blogs."
+	end
 end
--- commentdashboard/spec/request/home_spec.rb
+++ commentdashboard/spec/request/home_spec.rb
@@ -13,6 +13,20 @@ describe "Home Page" do
 	  	  expect(page.status_code).to be(200)
 	    end
 
+	    it "populates title" do
+	    	expect(page).to have_title 'Comments Dashboard'
+	    end
+
+	    describe "masthead" do
+	    	it "displays title"do
+	    	  expect(page).to have_selector 'h1', text:'Comments Dashboard'
+	      end
+	      
+	      it "displays subtitle" do
+	        expect(page).to have_selector 'h2', text:"Read comments from your favourite blogs."
+	      end
+	    end
+
     end
   end
 end
