79d59d0df2bac09d98f63d7d393da4319c81356a
Eric Brooke <ericbrooke@mac.com>
2014-01-19 12:39:54 -0800

    test blank state

--- commentdashboard/spec/request/home_spec.rb
+++ commentdashboard/spec/request/home_spec.rb
@@ -2,11 +2,17 @@ require 'spec_helper'
 
 describe "Home Page" do
 	describe "GET /" do
+    
+    context "empty database" do
 
-	  it "render success" do
-	  	visit "/"
-	  	expect(page.status_code).to be(200)
-	  end
+      before(:each) do
+        visit "/"
+      end
 
+	    it "render success" do
+	  	  expect(page.status_code).to be(200)
+	    end
+
+    end
   end
 end
