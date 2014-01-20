166a5c065312af5a0a6c8f3d641bf838d554cf92
Eric Brooke <ericbrooke@mac.com>
2014-01-19 16:43:17 -0800

    added shoulda gem, validating attributes of blog model

--- commentdashboard/app/models/blog.rb
+++ commentdashboard/app/models/blog.rb
@@ -1,2 +1,7 @@
 class Blog < ActiveRecord::Base
+
+	validates :title, presence: true, uniqueness: true
+	validates :comments_feed_url, presence: true, uniqueness: true
+	
+
 end
--- commentdashboard/spec/models/blog_spec.rb
+++ commentdashboard/spec/models/blog_spec.rb
@@ -4,11 +4,18 @@ describe Blog do
 
   let(:blog) { Blog.new(title: "My Blog", comments_feed_url:"http://example./comments/feed") }
 
-  it "saves attributes" do
-    
-    blog.save!
-    expect(blog).to be_valid
-  end
+  describe "attributes" do
 
+  	it { expect(blog).to validate_presence_of :title }
+  	it { expect(blog).to validate_uniqueness_of :title }
+
+  	it { expect(blog).to validate_presence_of :comments_feed_url }
+  	it { expect(blog).to validate_uniqueness_of :comments_feed_url }
+
+    it "saves attributes" do
+      blog.save!
+      expect(blog).to be_valid
+    end
+  end
 	
 end
