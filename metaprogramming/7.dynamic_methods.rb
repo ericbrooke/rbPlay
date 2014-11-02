002be62af35c0956497cd03d0d511fba07ee901b
Eric Brooke <ericbrooke@mac.com>
2014-11-02 15:29:13 -0800

    dynamic dispatch and defining methods dynamically

--- metaprogramming/7.dynamic_methods.rb
+++ metaprogramming/7.dynamic_methods.rb
@@ -6,3 +6,31 @@ end
 
 obj = MyClass.new
 obj.my_method(3)
+
+obj.send(:my_method, 3)
+
+# example of dynamic dispatch
+
+def update_attributes(options = {})
+  defaults = {}
+  attributes = [:strength, :constitution, :dexterity, :inteliigence, :wisdom, :charisma]
+
+  attributes.each do |attribute|
+    defaults[attribute] = Character.send attribute
+  end
+
+  defaults.merge!(options).each do |key, value|
+    send("#{key}=", value) if respond_to?("#{key}=")
+  end
+end
+
+# Define methods dynamically
+
+class MyClass
+  define_method :my_method do |my_arg|
+    my_arg * 3
+  end
+end
+
+obj = MyClass.new
+obj.my_method(2)
