cf2a432fdb39e509ee94b131707ce699b4107ad6
Eric Brooke <ericbrooke@mac.com>
2014-11-03 17:56:45 -0800

    added example of dynamic dispatching, dynamic methods and introspection

--- metaprogramming/6.duplicates.rb
+++ metaprogramming/6.duplicates.rb
@@ -23,3 +23,76 @@ class Computer
 end
 
 # can use dynamic methods and method_missing to remove the duplication
+
+
+# with dynamic dispatching
+
+class Computer
+  def initialize(computer_id, data_source)
+    @id = computer_id
+    @data_source = data_source
+  end
+
+  def mouse
+    component :mouse
+  end
+
+  def cpu
+    component :cpu
+  end
+
+  def keyboard
+    component :keyboard
+  end
+
+  def component(name)
+    info = @data_source.send "get_#{name}_info", @id
+    price = @data_source.send "get_#{name}_price", @id
+    result = "#{name.capitalize}: #{info} ($#{price})"
+    return "* #{result}" if price >= 100
+    result
+  end
+end
+
+# with dynamic methods and dynamic dispatching
+
+class Computer
+  def initialize(computer_id, data_source)
+    @id = computer_id
+    @data_source = data_source
+  end
+
+  def self.define_component(name)
+    define_method(name) do
+      info = @data_source.send "get_#{name}_info", @id
+      price = @data_source.send "get_#{name}_price", @id
+      result = "#{name.capitalize}: #{info} ($#{price})"
+      return "* #{result}" if price >= 100
+      result
+    end
+  end
+
+  define_component :mouse
+  define_component :cpu
+  define_component :keyboard
+end
+
+# with introspection added
+
+class Computer
+  def initialize(computer_id, data_source)
+    @id = computer_id
+    @data_source = data_source
+    data_source.methods.grep(/^get(.*)_info$/) { Computer.define_component $1}
+  end
+
+  def self.define_component(name)
+    define_method(name) do
+      info = @data_source.send "get_#{name}_info", @id
+      price = @data_source.send "get_#{name}_price", @id
+      result = "#{name.capitalize}: #{info} ($#{price})"
+      return "* #{result}" if price >= 100
+      result
+    end
+  end
+end
